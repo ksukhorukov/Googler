c3bfa54ae033e3a74591a307fb50edc6e37c8239
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-10-11 18:21:00 +0300

    refactored queries controller

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -5,17 +5,19 @@ class QueriesController < ApplicationController
   include StatisticsQueries
 
   def create
-    pp params
     type = params['type']
-    if type == 'query1'
-      puts "Result:" + how_many_urls_contain_the_word_in_adwords(params['word']).to_s
-      render 'result', locals: { result: how_many_urls_contain_the_word_in_adwords(params['word']) }
-    elsif type == 'query2'
-      render 'result', locals: { result: how_many_times_specific_url_shows_up_in_seo(params['url']) }
-    elsif type == 'query3'
-      render 'result', locals: { result: 'test result' }
-    else
+    unless %w[query1 query2 query3].include? type
       render :new
+    else
+      result  = ''
+      if type == 'query1'
+        result = how_many_urls_contain_the_word_in_adwords(params['word'])
+      elsif type == 'query2'
+        result = how_many_times_specific_url_shows_up_in_seo(params['url']) 
+      elsif type == 'query3'
+        result = 'test result'
+      end
+      render 'result', locals: { result: result  }
     end
   end
 
