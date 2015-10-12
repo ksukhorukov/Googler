70a77b7fba5d7aa538df85964d19f232b71bb29b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-10-12 11:38:02 +0300

    Added some more statistical methods

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -6,7 +6,7 @@ class QueriesController < ApplicationController
 
   def create
     type = params['type']
-    unless %w[query1 query2 query3].include? type
+    unless %w[query1 query2 query3 query4].include? type
       render :index
     else
       result  = ''
@@ -15,9 +15,13 @@ class QueriesController < ApplicationController
       elsif type == 'query2'
         result = how_many_times_specific_url_shows_up_in_seo(params['url']) 
       elsif type == 'query3'
-        result = 'test result'
+        result = how_many_urls_have_two_or_more_slash_characters
+      elsif type == 'query4'
+        result = how_many_urls_have_one_or_more_caret_charecter
+        pp how_many_urls_have_one_or_more_caret_charecter
+        puts "Result: #{how_many_urls_have_one_or_more_caret_charecter}"
       end
-      render 'result', locals: { result: result  }
+      render 'result', locals: { result: result }
     end
   end
 
--- app/lib/statistics_queries.rb
+++ app/lib/statistics_queries.rb
@@ -1,17 +1,5 @@
 module StatisticsQueries
 
-    #   t.integer "keyword_id"
-    # t.integer "adw_top"
-    # t.integer "adw_right"
-    # t.integer "adw_total"
-    # t.string  "top_urls"
-    # t.string  "right_urls"
-    # t.integer "non_adw"
-    # t.string  "urls_non_adw"
-    # t.integer "links_total"
-    # t.string  "total_results"
-    # t.string  "word"
-
   def how_many_urls_contain_the_word_in_adwords(word)
     counter = 0
     statistics = Statistic.all
@@ -28,6 +16,36 @@ module StatisticsQueries
     statistics.each do |stats|
       counter += stats.top_urls.scan(/#{url}/i).count
       counter += stats.right_urls.scan(/#{url}/i).count
+    end
+    counter
+  end
+
+  def how_many_urls_have_two_or_more_slash_characters
+    counter = 0
+    urls = ''
+    statistics = Statistic.all
+    statistics.each do |stats|
+      urls = stats.top_urls.gsub("http:\/\/",'') + ',' + urls
+      urls = stats.right_urls.gsub("http:\/\/",'') + ',' + urls
+      urls = stats.urls_non_adw.gsub("http:\/\/",'') + ',' + urls
+    end
+    urls.split(',').each do |url|
+      counter += 1 if url.scan(/\//).count >= 2  unless url.nil?   
+    end
+    counter
+  end
+
+  def how_many_urls_have_one_or_more_caret_charecter
+    counter = 0
+    urls = ''
+    statistics = Statistic.all
+    statistics.each do |stats|
+      urls = stats.top_urls + ',' + urls
+      urls = stats.right_urls + ',' + urls
+      urls = stats.urls_non_adw + ',' + urls
+    end
+    urls.split(',').each do |url|
+      counter += 1 if url.scan(/>/).count >= 1 unless url.nil?
     end
     counter
   end
